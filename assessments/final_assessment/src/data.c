6d532d4ee12edb8895fdb710df4207b32bd431f0
Tran Huy Hung <kids90@gmail.com>
2024-11-29 13:32:28 +0000

    Function Implementations for Course 1 Final Assessment

--- assessments/final_assessment/src/data.c
+++ assessments/final_assessment/src/data.c
@@ -4,10 +4,84 @@
 /***********************************************************
  Function Definitions
 ***********************************************************/
-uint8_t my_itoa(int32_t data, uint8_t * ptr, uint32_t base) {
-	return 0;
+
+uint8_t my_itoa(int32_t data, uint8_t* ptr, uint32_t base) {
+    if (base < 2 || base > 16) {
+        return 0;
+    }
+
+    uint8_t length = 0;
+    uint8_t isNegative = 0;
+
+    if (data < 0 && base == 10) {
+        isNegative = 1;
+        data = -data;
+    }
+
+    uint8_t* start_ptr = ptr;
+    do {
+        int remainder = data % base;
+        data /= base;
+
+        if (remainder < 10) {
+            *ptr = '0' + remainder;
+        }
+        else {
+            *ptr = 'A' + (remainder - 10);
+        }
+        ptr++;
+        length++;
+    } while (data != 0);
+
+    if (isNegative) {
+        *ptr = '-';
+        ptr++;
+        length++;
+    }
+
+    *ptr = '\0';
+    length++;
+
+    // Reverse the string
+    uint8_t* end_ptr = ptr - 1;
+    ptr = start_ptr;
+    while (ptr < end_ptr) {
+        uint8_t temp = *ptr;
+        *ptr = *end_ptr;
+        *end_ptr = temp;
+
+        ptr++;
+        end_ptr--;
+    }
+
+    return length;
 }
 
-int32_t my_atoi(uint8_t * ptr, uint8_t digits, uint32_t base) {
-	return 0;
+
+int32_t my_atoi(uint8_t* ptr, uint8_t digits, uint32_t base) {
+    if (base < 2 || base > 16) {
+        return -1;
+    }
+
+    int32_t res = 0;
+    uint8_t isNegative = 0;
+    int i = 0;
+    while (i < digits) {
+        if (*ptr == '-') {
+            isNegative = 1;
+        }
+        else if (*ptr >= '0' && *ptr <= '9') {
+            res = res * base + (*ptr - '0');
+        }
+        else if (*ptr >= 'A' && *ptr <= 'F') {
+            res = res * base + (*ptr - 'A' + 10);
+        }
+
+        ptr++;
+        i++;
+    }
+
+    if (isNegative) res = -res;
+
+    return res;
 }
--- assessments/final_assessment/src/memory.c
+++ assessments/final_assessment/src/memory.c
@@ -48,10 +48,105 @@ void clear_all(char * ptr, unsigned int size){
   set_all(ptr, 0, size);
 }
 
-uint8_t * my_memmove(uint8_t * src, uint8_t * dst, size_t length) {return 0;}
-uint8_t * my_memcopy(uint8_t * src, uint8_t * dst, size_t length) {return 0;} 
-uint8_t * my_memset(uint8_t * src, size_t length, uint8_t value) {return 0;}
-uint8_t * my_memzero(uint8_t * src, size_t length) {return 0;}
-uint8_t * my_reverse(uint8_t * src, size_t length) {return 0;}
-int32_t * reserve_words(size_t length) {return 0;}
-void free_words(int32_t * src) {}
+
+
+uint8_t* my_memmove(uint8_t* src, uint8_t* dst, size_t length) {
+    if (src == NULL || dst == NULL) {
+        return dst;
+    }
+
+    if (dst > src && dst < src + length) {
+        // Overlapping, copy backward
+        src += length - 1;
+        dst += length - 1;
+        while (length--) {
+            *dst-- = *src--;
+        }
+    }
+    else {
+        // Non-overlapping or src is ahead of dst, copy forward
+        while (length--) {
+            *dst++ = *src++;
+        }
+    }
+
+    return dst;
+}
+
+uint8_t* my_memcopy(uint8_t* src, uint8_t* dst, size_t length) {
+    if (src == NULL || dst == NULL) {
+        return dst;
+    }
+
+    while (length--) {
+        *dst++ = *src++;
+    }
+
+    return dst;
+}
+
+uint8_t* my_memset(uint8_t* src, size_t length, uint8_t value) {
+    if (src == NULL) {
+        return src;
+    }
+
+    while (length--) {
+        *src++ = value;
+    }
+
+    return src;
+}
+
+uint8_t* my_memzero(uint8_t* src, size_t length) {
+    if (src == NULL) {
+        return src;
+    }
+
+    while (length--) {
+        *src++ = 0;
+    }
+
+    return src;
+}
+
+uint8_t* my_reverse(uint8_t* src, size_t length) {
+    if (src == NULL || length <= 1) {
+        return src;
+    }
+
+    uint8_t* start = src;
+    uint8_t* end = src + length - 1;
+
+    while (start < end) {
+        uint8_t temp = *start;
+        *start = *end;
+        *end = temp;
+
+        start++;
+        end--;
+    }
+
+    return src;
+}
+
+int32_t* reserve_words(size_t length) {
+    if (length == 0) {
+        return NULL;
+    }
+
+    int32_t* memory = (int32_t*)malloc(length * sizeof(int32_t));
+    if (memory == NULL) {
+        return NULL;
+    }
+
+    return memory;
+}
+
+void free_words(int32_t* src) {
+    if (src == NULL) {
+        return;
+    }
+
+    free(src);
+}
+
